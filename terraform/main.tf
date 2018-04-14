0a9d2144dd9882055932e71a7019538331ea4bd0
Jens Peter Secher <jpsecher@lundogbendsen.dk>
2018-04-14 12:07:19 +0200

    Datastores.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,27 @@
 provider "vsphere" {
   version         = "~> 1.4"
-  user            = "root"
+  user            = "${var.vsphere-user}"
   password        = "${var.vsphere-password}"
-  vsphere_server  = "esxi"
+  vsphere_server  = "${var.vsphere-server}"
   allow_unverified_ssl = true
 }
 
-data "vsphere_datacenter" "datacenter" {}
+data "vsphere_datacenter" "dc" {}
+data "vsphere_datastore" "ds1" {
+  name = "${var.vsphere-datastore-1}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+data "vsphere_datastore" "ds2" {
+  name = "${var.vsphere-datastore-2}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
 
 output "datacenter" {
-  value = "${data.vsphere_datacenter.datacenter.id}"
+  value = "${data.vsphere_datacenter.dc.id}"
+}
+output "datastore-1" {
+  value = "${data.vsphere_datastore.ds1.id}"
+}
+output "datastore-2" {
+  value = "${data.vsphere_datastore.ds2.id}"
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "vsphere-server" {
+  default = "esxi"
+}
+variable "vsphere-datastore-1" {
+  default = "datastore1"
+}
+variable "vsphere-datastore-2" {
+  default = "datastore2"
+}
+variable "vsphere-user" {
+  default = "root"
+}
